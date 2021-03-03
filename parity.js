9d027fce3d8304120d0fbfd83e02dc4329d9c937
Aeron01 <one.soulreaper@gmail.com>
2021-03-03 16:59:50 +0100

    exo 5 done

--- parity.js
+++ parity.js
@@ -1,7 +1,7 @@
 // Ecrire une fonction isOdd qui prend un nombre en paramètre et qui devra retourner true si le nombre passé en paramètre est impair, sinon la fonction retournera false.
 
-const isOdd = (nb1, nb2) => {
-  if (nb1 
-  }
-
-console.log(parite())
+const isOdd = (nb) => {
+  return (nb & 2) ? false : true;
+}
+let nb = 13
+console.log(isOdd(nb))
