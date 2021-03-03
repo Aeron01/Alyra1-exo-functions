1e87e732399e2fe7be07519a017c4fc43808bad7
Aeron01 <one.soulreaper@gmail.com>
2021-03-03 19:41:09 +0100

    update

--- parity.js
+++ parity.js
@@ -1,7 +1,8 @@
 // Ecrire une fonction isOdd qui prend un nombre en paramètre et qui devra retourner true si le nombre passé en paramètre est impair, sinon la fonction retournera false.
 
 const isOdd = (nb) => {
-  return (nb % 2 === 0) ? false : true;
+  nb = (nb % 2 === 0) ? false : true;
+  return `This number is odd ? ${nb}`
 }
-let nb = 13
+let nb = 12
 console.log(isOdd(nb))
