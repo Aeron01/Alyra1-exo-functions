ba237ba6dbe4a8ad865ffa27cf2c18f80e35cda1
Aeron01 <one.soulreaper@gmail.com>
2021-03-04 15:01:08 +0100

    done

---
+++ convertMiToKm.js
@@ -0,0 +1,7 @@
+// Ecrire une fonction convertMiToKm qui fait la conversion d'une distance exprimée en Milles, en Kilomètres. 1 Mille est à égal à 1.60934 Kilomètres.
+
+const convertMiToKm = (nb) => {
+  return `{${nb} Milles est égale à ${1.60934 * nb} Km.`
+}
+
+console.log(convertMiToKm(5))
