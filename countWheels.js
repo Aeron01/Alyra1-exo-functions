a143ff9d0cd37bb3b80934fae0263c425b49dab7
Aeron01 <one.soulreaper@gmail.com>
2021-03-05 12:16:21 +0100

    corrected

--- countWheels.js
+++ countWheels.js
@@ -9,3 +9,18 @@ const countWheels = (nb_monocycle, nb_moto, nb_voiture, nb_limousine) => {
 console.log(countWheels(10, 20, 7, 2)) // returns 90
 console.log(countWheels(2, 3, 4, 5))  // returns 54
 console.log(countWheels(0, 70, 12, 8))  // returns 236
+
+
+// correction
+const NB_WHEELS_MONOCYCLE = 1
+const NB_WHEELS_CYCLE = 2
+const NB_WHEELS_CAR = 4
+const NB_WHEELS_LIMO = 6
+
+const countWheels2 = (nb_monocycles, nb_motos, nb_voitures, nb_limousines) => {
+  return nb_monocycles * NB_WHEELS_MONOCYCLE + nb_motos * NB_WHEELS_CYCLE + nb_voitures * NB_WHEELS_CAR + nb_limousines * NB_WHEELS_LIMO
+}
+
+console.log(countWheels2(10, 20, 7, 2)) // returns 90
+console.log(countWheels2(2, 3, 4, 5))  // returns 54
+console.log(countWheels2(0, 70, 12, 8))  // returns 236
