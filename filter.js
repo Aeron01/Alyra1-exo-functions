908f219a414b142b30904f7f95fe7326f37ea34d
Aeron01 <one.soulreaper@gmail.com>
2021-03-05 12:53:25 +0100

    updatted

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
-// correction de l'exo (bizzare, marche pas)
+// correction de l'exo
 
-const isEven = (number) => {
-  return number % 2 === 0 ? true : false
+const isEven = (numbers) => {
+  return numbers % 2 === 0 ? true : false
 }
 
 const filter = (func, array) => {
@@ -11,11 +11,13 @@ const filter = (func, array) => {
       filteredTab.push(elem)
     }
   }
+  return filteredTab
 }
 
 
-let number = [12, 1, 43, 3, 8, 9, 22, 6, 19, 88, 76]
+number = [12, 1, 43, 3, 8, 9, 22, 6, 19, 88, 76]
 
+console.log(filter(isEven, number))
 
 //let newTab = number.filter(isEven)
 //console.log(newTab)
